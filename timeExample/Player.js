8f8fb9d53262318ffd2901a012788dc93e0fe82c
Phildo <phildo211@gmail.com>
2011-10-25 12:45:00 -0500

    Updated timeExample to record multiple clones and store them sensibly. Currently, there is no playback, but it could be easily done with a 'draw' function.
    Note- any collision detection must be applied to the current player AND
    the list of clones. The clone is simply a bundle of what buttons the
    user has pressed at any given time.

--- timeExample/Player.js
+++ timeExample/Player.js
@@ -24,6 +24,7 @@ function Player(){
 	this.mLeft = false;
 	this.mRight = false;
 	this.jump = false;
+	this.dead = false;
 
 	this.duplicate = function(){
 		var dup = new Player();
@@ -33,43 +34,29 @@ function Player(){
 		dup.mLeft = this.mLeft;
 		dup.mRight = this.mRight;
 		dup.jump = this.jump;
+		dup.dead = this.dead;
 		return dup;
 	}
 }
 
 //Pass in position, velocity, and time shot
 function Shot(px, py, pz, vx, vy, vz, t){
-	this.time = t;
 	this.pos = new Vector(px, py, pz);
-	this.org = new Vector(px, py, pz); //Origin of shot- DOES NOT CHANGE
 	this.vel = new Vector(vx, vy, vz);
-	this.go = false;
 
 	this.updatePos = function() {
 		this.pos.setVect3(this.pos.x+this.vel.x, this.pos.y+this.vel.y, this.pos.z+this.vel.z);
 	}
-
-	this.resetShot = function() {
-		this.pos.setVect3(this.org.x, this.org.y, this.org.z);
-		this.stop = false;
-	}
 }
 
 function Record(){
-	this.recordDead = false;
 	this.ts = new Array(); //ts = 'timeSlice'. An array of 'Vector3's.
-	this.shots = new Array(); //an array of 'Shot's
 	this.p = 0; //Current playback location. Think playhead.
 
 	//Pass in a player, this adds the players pos vector to its timeslice array
 	this.addSlice = function(pl) {
 		this.ts[this.p]=pl.duplicate();
 		this.p++;
-	}
-
-	//Pass in a shot
-	this.addShot = function(s) {
-		this.shots[this.shots.length] = s;
 	}
 
 	//Resets playhead to 0
@@ -79,18 +66,8 @@ function Record(){
 
 	//Gets next frame to play. If no frames left, returns last available
 	this.playNextSlice = function() {
-//		this.updateShots();
 		this.p++;
 		if(this.p > this.ts.length) this.p = this.ts.length;
 		return this.ts[this.p-1];
-	}
-
-	this.updateShots = function() {
-		for(var x = 0; x < this.shots.length; x++){
-			if((!playerDead && this.shots[x].t == p) || (this.shots[x].go && this.shots[x].t>p)){
-				this.shots[x].go = true;
-				this.shots[x].updatePos();
-			}
-		}
 	}
 }
