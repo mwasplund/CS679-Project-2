0c8bafef33b826d713160e525570cb3ee1fea508
Phildo <phildo211@gmail.com>
2011-10-27 14:06:52 -0500

    Updated player in main folder to mirror that of player in timeExample Foder

--- Player.js
+++ Player.js
@@ -1,75 +1,142 @@
-function Vector3(a,b,c){
-	this.x;
-	this.y;
-	this.z;
-
-	this.setVect3 = function(a,b,c){
-		this.x = a;
-		this.y = b;
-		this.z = c;
-	}
-	
-	this.duplicate = function(){
-		var dup = new Vector3(this.x, this.y, this.z);
-		return dup;
-	}
-
-	this.setVect3(a,b,c);
-}
+// Constants
+var Player_MoveSpeed = 0.5;
+var Player_RotateSpeed = 2.5;
 
 function Player(){
-	this.pos = new Vector3(0,0,0);
+	this.pos = vec3.create([0,0,0]);
+	this.lookat = vec3.create([0,0,1]);
+	this.dir = vec3.create([0, 0, 1]);
+	this.dir_Up = vec3.create([0,1,0]);
+	this.pos_Up = vec3.create([0,1,0]);
+	this.yaw = 0;
+	this.pitch = 0;
+	this.rRight = false;
+	this.rLeft = false;
+	this.rUp = false;
+	this.rDown = false;
 	this.mForward = false;
 	this.mBackward = false;
 	this.mLeft = false;
 	this.mRight = false;
 	this.jump = false;
+	this.dead = false;
 
 	this.duplicate = function(){
 		var dup = new Player();
-		dup.pos = this.pos.duplicate();
+		dup.pos = vec3.create([this.pos[0], this.pos[1], this.pos[2]]);
+		dup.lookat = vec3.create([this.lookat[0], this.lookat[1], this.lookat[2]]);
+		dup.dir = vec3.create([this.dir[0], this.dir[1], this.dir[2]]);
+		dup.dir_Up = vec3.create([this.dir_Up[0], this.dir_Up[1], this.dir_Up[2]]);
+		dup.pos_Up =vec3.create([this.pos_Up[0], this.pos_Up[1], this.pos_Up[2]]);
+		dup.yaw = this.yaw;
+		dup.pitch = this.pitch; 
+		dup.rRight = this.rRight;
+		dup.rLeft = this.rLeft;
+		dup.rUp = this.rUp;
+		dup.rDown = this.rDown;
 		dup.mForward = this.mForward;
 		dup.mBackward = this.mBackward;
 		dup.mLeft = this.mLeft;
 		dup.mRight = this.mRight;
 		dup.jump = this.jump;
+		dup.dead = this.dead;
 		return dup;
+	}
+	
+	this.Update = function(){
+	  var Changed = false;
+	  if(this.mLeft)     
+	  {
+	    // Caluculate Direction forward
+	    vec3.direction(this.pos, this.lookat, this.dir);
+	    
+	    // Calculate the Direction Up
+	    vec3.add(this.pos, [0,1,0], this.pos_Up);
+	    vec3.direction(this.pos_Up, this.pos, this.dir_Up)
+	    
+	    // Calculate direction right
+	    vec3.cross(this.dir_Up, this.dir, this.dir);
+	    
+	    // Move in direction
+	    vec3.scale(this.dir, Player_MoveSpeed);
+	    vec3.subtract(this.pos, this.dir)
+	    Changed = true;
+	  }
+ 		if(this.mRight)     
+	  {
+	    // Caluculate Direction forward
+	    vec3.direction(this.pos, this.lookat, this.dir);
+	    
+	    // Calculate the Direction Up
+	    vec3.add(this.pos, [0,1,0], this.pos_Up);
+	    vec3.direction(this.pos_Up, this.pos, this.dir_Up)
+	    
+	    // Calculate direction right
+	    vec3.cross(this.dir_Up, this.dir, this.dir);
+	    
+	    // Move in direction
+	    vec3.scale(this.dir, Player_MoveSpeed);
+	    vec3.add(this.pos, this.dir)
+	    Changed = true;
+	  }
+    if(this.mForward)     
+	  {
+	    vec3.direction(this.lookat, this.pos, this.dir);
+	    vec3.scale(this.dir, Player_MoveSpeed);
+	    vec3.add(this.pos, this.dir)
+	    Changed = true;
+	  }
+    if(this.mBackward)     
+	  {
+	    vec3.direction(this.lookat, this.pos, this.dir);
+	    vec3.scale(this.dir, Player_MoveSpeed);
+	    vec3.subtract(this.pos, this.dir)
+	    Changed = true;
+	  }
+ 		
+ 		if(this.rRight) 
+ 		{
+ 		  this.yaw += Player_RotateSpeed;
+ 		  Changed = true;
+ 		  if(this.yaw > 360.0)
+ 		    this.yaw -= 360.0;
+ 		}
+ 		if(this.rLeft) 
+ 		{
+ 		  this.yaw -= Player_RotateSpeed;
+ 		  Changed = true;
+ 		  if(this.yaw < 0.0)
+ 		    this.yaw += 360.0;
+ 		}
+
+ 		
+ 		// Set the Lookat to right in front of the player's eyes
+ 		if(Changed)
+ 		{
+ 		  var Percent_Yaw =  -degToRad(this.yaw);
+ 		  vec3.add(this.pos, [Math.sin(Percent_Yaw), 0, Math.cos(Percent_Yaw)], this.lookat);
+ 		}
 	}
 }
 
 //Pass in position, velocity, and time shot
 function Shot(px, py, pz, vx, vy, vz, t){
-	this.time = t;
 	this.pos = new Vector(px, py, pz);
-	this.org = new Vector(px, py, pz); //Origin of shot- DOES NOT CHANGE
 	this.vel = new Vector(vx, vy, vz);
-	this.go = false;
 
 	this.updatePos = function() {
 		this.pos.setVect3(this.pos.x+this.vel.x, this.pos.y+this.vel.y, this.pos.z+this.vel.z);
 	}
-
-	this.resetShot = function() {
-		this.pos.setVect3(this.org.x, this.org.y, this.org.z);
-		this.stop = false;
-	}
 }
 
 function Record(){
-	this.recordDead = false;
 	this.ts = new Array(); //ts = 'timeSlice'. An array of 'Vector3's.
-	this.shots = new Array(); //an array of 'Shot's
 	this.p = 0; //Current playback location. Think playhead.
 
 	//Pass in a player, this adds the players pos vector to its timeslice array
 	this.addSlice = function(pl) {
 		this.ts[this.p]=pl.duplicate();
 		this.p++;
-	}
-
-	//Pass in a shot
-	this.addShot = function(s) {
-		this.shots[this.shots.length] = s;
 	}
 
 	//Resets playhead to 0
@@ -79,18 +146,8 @@ function Record(){
 
 	//Gets next frame to play. If no frames left, returns last available
 	this.playNextSlice = function() {
-//		this.updateShots();
 		this.p++;
 		if(this.p > this.ts.length) this.p = this.ts.length;
 		return this.ts[this.p-1];
-	}
-
-	this.updateShots = function() {
-		for(var x = 0; x < this.shots.length; x++){
-			if((!playerDead && this.shots[x].t == p) || (this.shots[x].go && this.shots[x].t>p)){
-				this.shots[x].go = true;
-				this.shots[x].updatePos();
-			}
-		}
 	}
 }
